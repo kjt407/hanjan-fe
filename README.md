9dde815e489ef5c625955073139d9edc15e29264
kjt407 <kjt40700@gmail.com>
2022-10-31 21:31:11 +0900

    vue cli 환경으로 재구성

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# 프로젝트 한잔 (FE)
+# hanjan-fe
 
-## 개요
-hanjan restful api를 활용한 FE구성
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -1,31 +1,12 @@
-/*!
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-=========================================================
-* BootstrapVue Argon Dashboard - v1.0.0
-=========================================================
+Vue.config.productionTip = false;
 
-* Product Page: https://www.creative-tim.com/product/bootstrap-vue-argon-dashboard
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Coded by www.creative-tim.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import Vue from 'vue';
-import DashboardPlugin from './plugins/dashboard-plugin';
-import App from './App.vue';
-
-// router setup
-import router from './routes/router';
-// plugin setup
-Vue.use(DashboardPlugin);
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App),
-  router
-});
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
